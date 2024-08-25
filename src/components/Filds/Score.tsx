1858837c19ba0d31c10f00e4a72c3d577fcc13ea
xxlvt <peachhomeless@gmail.com>
2024-08-25 06:16:45 +0300

    create Score component

--- src/components/Filds/Score.tsx
+++ src/components/Filds/Score.tsx
@@ -1,20 +1,23 @@
-import './Score.css'
 import {FC} from "react";
+import './Score.css';
 
 interface ScoreProps {
-    label: string
-    value: number
+    label: string;
+    value: number;
 }
 
 export const Score: FC<ScoreProps> = ({label, value}) => {
-    return(
+    return (
         <div className={'score'}>
             <div className={'score-label'}>
                 {label}
             </div>
             <div className={'score-value'}>
-                {value}
+                {(value > 999999999) ?
+                    '999.999.999+' :
+                    String(value).replace(/(?:(^\d{1,3})(?=(?:\d{3})*$)|(\d{3}))(?!$)/mg, '$1$2.')
+                }
             </div>
         </div>
-    )
-}
+    );
+};
